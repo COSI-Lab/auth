ca245a77d98351614b08b5a9263a1476f1d62834
Alextopher <christopher.mahoney2000@gmail.com>
2022-09-05 14:05:39 -0400

    wrap some db context functions in Option

--- authd/src/db.rs
+++ authd/src/db.rs
@@ -2,13 +2,13 @@ use crate::types::{Group, Passwd, Shadow};
 
 pub trait DBContext {
     fn get_all_groups() -> Vec<Group>;
-    fn get_group_by_name(name: String) -> Group;
-    fn get_group_by_gid(gid: u32) -> Group;
+    fn get_group_by_name(name: String) -> Option<Group>;
+    fn get_group_by_gid(gid: u32) -> Option<Group>;
 
     fn get_all_passwd() -> Vec<Passwd>;
-    fn get_passwd_by_name(name: String) -> Passwd;
-    fn get_passwd_by_uid(uid: u32) -> Passwd;
+    fn get_passwd_by_name(name: String) -> Option<Passwd>;
+    fn get_passwd_by_uid(uid: u32) -> Option<Passwd>;
 
     fn get_all_shadow() -> Vec<Shadow>;
-    fn get_shadow_by_name(name: String) -> Shadow;
+    fn get_shadow_by_name(name: String) -> Option<Shadow>;
 }
