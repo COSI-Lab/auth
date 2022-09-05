d569f331ad41de2adc8176fb8a2ff4311256cee5
ember arlynx <ember@lunar.town>
2022-09-05 15:10:03 -0400

    results

--- authd/src/db.rs
+++ authd/src/db.rs
@@ -1,14 +1,17 @@
 use crate::types::{Group, Passwd, Shadow};
 
+pub type DBErr = ();
+pub type Result<T> = std::result::Result<T, DBErr>;
+
 pub trait DBContext {
-    fn get_all_groups() -> Vec<Group>;
-    fn get_group_by_name(name: String) -> Option<Group>;
-    fn get_group_by_gid(gid: u32) -> Option<Group>;
+    fn get_all_groups(&self) -> Result<Vec<Group>>;
+    fn get_group_by_name(&self,name: String) -> Result<Option<Group>>;
+    fn get_group_by_gid(&self,gid: u32) -> Result<Option<Group>>;
 
-    fn get_all_passwd() -> Vec<Passwd>;
-    fn get_passwd_by_name(name: String) -> Option<Passwd>;
-    fn get_passwd_by_uid(uid: u32) -> Option<Passwd>;
+    fn get_all_passwd(&self) -> Result<Vec<Passwd>>;
+    fn get_passwd_by_name(&self,name: String) -> Result<Option<Passwd>>;
+    fn get_passwd_by_uid(&self,uid: u32) -> Result<Option<Passwd>>;
 
-    fn get_all_shadow() -> Vec<Shadow>;
-    fn get_shadow_by_name(name: String) -> Option<Shadow>;
+    fn get_all_shadow(&self) -> Result<Vec<Shadow>>;
+    fn get_shadow_by_name(&self,name: String) -> Result<Option<Shadow>>;
 }
