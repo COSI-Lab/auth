c4dc2b9ca0567ec5bc15462c54a1122a5e7b1a9e
ember arlynx <ember@lunar.town>
2022-09-06 14:51:03 -0400

    opaque_ke ksf

--- authd/src/rpc.rs
+++ authd/src/rpc.rs
@@ -16,8 +16,7 @@ impl CipherSuite for DefaultCipherSuite {
     type OprfCs = opaque_ke::Ristretto255;
     type KeGroup = opaque_ke::Ristretto255;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
-
-    type Ksf = argon2::Argon2<'static>;
+    type Ksf = opaque_ke::ksf::Identity;
 }
 
 #[derive(Deserialize, Serialize, Debug)]
